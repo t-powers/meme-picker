c9a9018f80dca3d8d32e141ccb181c64074c5df3
t-powers <127955989+t-powers@users.noreply.github.com>
2024-07-31 20:50:12 -0400

    Refactor radio listener

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 
-emotionRadios.addEventListener("change", (e) => {
-  console.log(e.target.id);
-});
+emotionRadios.addEventListener("change", highlightCheckedOption);
+
+function highlightCheckedOption(e) {
+  document.getElementById(e.target.id).classList.add("highlight");
+}
 
 function getEmotionsArray(cats) {
   const emotionsArray = [];
