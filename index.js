00af56808c960f9530a5aa064f078a1f773c9634
t-powers <127955989+t-powers@users.noreply.github.com>
2024-07-31 20:33:39 -0400

    Target radio buttons

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 
+emotionRadios.addEventListener("change", (e) => {
+  console.log(e.target.id);
+});
+
 function getEmotionsArray(cats) {
   const emotionsArray = [];
   for (let cat of cats) {
