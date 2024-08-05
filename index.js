b084b3cbb571c232d913898e3ec863dc917d13be
t-powers <127955989+t-powers@users.noreply.github.com>
2024-08-04 20:36:53 -0400

    Control img btn, gif checkbox, add eventlistener

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import { catsData } from "/data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
+const getImageBtn = document.getElementById("get-image-btn");
+const gifsOnlyOption = document.getElementById("gifs-only-option");
 
 emotionRadios.addEventListener("change", highlightCheckedOption);
+getImageBtn.addEventListener("click", getMatchingCatsArray);
+
+
+function getMatchingCatsArray() {
+  if (document.querySelector('input[type="radio"]:checked')) {
+    const gifChecked = document.querySelector(
+      "input[type='radio']:checked"
+    ).value;
+    const isGif = gifsOnlyOption.checked;
+    console.log(isGif);
+  }
+}
 
 function highlightCheckedOption(e) {
   document.getElementById(e.target.id).parentElement.classList.add("highlight");
